07af92382310356e92ddf17d97614469bfcf3977
FaizalFeng <fzx401@gmail.com>
2024-09-22 21:48:12 +0800

    ✅ 初步完成了Transformer Encoder和数据的重构

--- dl/transformer/Encoder.py
+++ dl/transformer/Encoder.py
@@ -3,39 +3,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class InputEmbedding(nn.Module):
-    def __init__(
-        self,
-        vocab_size: int,
-        embed_size: int,
-        max_seq_len: int = 128,
-        device: torch.device = "cpu",
-    ):
+class PosEncoding(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.input_embedding = nn.Embedding(vocab_size, embed_size, device=device)
-        self.max_seq_len = max_seq_len
 
-    def forward(self, input_x: torch.tensor):  # input_x: [batch_size, seq_len]
-        out = self.input_embedding(input_x)  # out: [batch_size, seq_len, embed_size]
-        return out
-
-
-class PositionalEncoding:
-    def __init__(self, embed_size: int, max_seq_len: int = 128):
-        self.embed_size = embed_size
-        self.max_seq_len = max_seq_len
-
-    def __call__(self, input_x: torch.tensor):
-        # if input_x.shape[1] <= self.max_seq_len:
-        #     input_x = F.pad(input_x, (0, self.max_seq_len - input_x.shape[1]))
-        # else:
-        #     input_x = input_x[..., self.max_seq_len]
-        return self._positional_encoding_vector(
-            input_x, self.embed_size
-        )  # [batch_size, seq_len, embed_size]
-
-    def _positional_encoding_vector(self, input_x: torch.tensor, embed_size: int):
-        batch_size, seq_len = input_x.shape[0], input_x.shape[1]
+    def forward(self, input_x: torch.tensor):
+        batch_size, seq_len, embed_size = input_x.shape
         pos_encoding = torch.zeros(batch_size, seq_len, embed_size)
         pos_encoding_element = torch.arange(0, seq_len)[..., None].tile(
             (1, embed_size)
@@ -45,36 +18,33 @@ class PositionalEncoding:
         )
         pos_encoding[..., 0::2] = torch.sin(pos_encoding_element[..., 0::2])
         pos_encoding[..., 1::2] = torch.cos(pos_encoding_element[..., 1::2])
-
-        return pos_encoding
-
-    def _positional_encoding(self, input_x: torch.tensor, embed_size: int):
-        batch_size, seq_len = input_x.shape[0], input_x.shape[1]
-        pos_encoding = torch.zeros(batch_size, seq_len, embed_size)
-        for i in range(embed_size):
-            if i % 2 == 0:
-                pos_encoding[:, :, i] = torch.sin(
-                    torch.arange(0, seq_len) / 10000 ** (i / embed_size)
-                )
-            else:
-                pos_encoding[:, :, i] = torch.cos(
-                    torch.arange(0, seq_len) / 10000 ** ((i - 1) / embed_size)
-                )
         return pos_encoding
 
 
 class InputBlock(nn.Module):
-    def __init__(self, vocab_size: int, embed_size: int, device: torch.device = "cpu"):
+    def __init__(self, vocab_size: int, embed_size: int):
         super().__init__()
-        self.device = device
-        self.input_embedding = InputEmbedding(vocab_size, embed_size, device=device)
-        self.positional_embedding = PositionalEncoding(embed_size)
+        self.input_embedding = nn.Embedding(vocab_size, embed_size)
+        # self.pos_embedding = PosEncoding()
+
+    def _positional_encoding(self, input_x: torch.tensor):
+        batch_size, seq_len, embed_size = input_x.shape
+        pos_encoding = torch.zeros(batch_size, seq_len, embed_size)
+        pos_encoding_element = torch.arange(0, seq_len)[..., None].tile(
+            (1, embed_size)
+        ) / (
+            10000
+            ** ((torch.arange(embed_size) // 2) / embed_size)[None, ...].tile(seq_len, 1)
+        )
+        pos_encoding[..., 0::2] = torch.sin(pos_encoding_element[..., 0::2])
+        pos_encoding[..., 1::2] = torch.cos(pos_encoding_element[..., 1::2])
+        return pos_encoding
 
     def forward(self, input_x: torch.tensor):
         # input_x: [batch_size, seq_len]
-        x = self.input_embedding(input_x) + self.positional_embedding(input_x).to(
-            self.device
-        )
+        x = self.input_embedding(input_x.long())  # x: [batch_size, seq_len, embed_size]
+        pos_embedding = self._positional_encoding(x).to(input_x.device)
+        x = x + pos_embedding
 
         return x
 
@@ -82,26 +52,30 @@ class InputBlock(nn.Module):
 class MultiHeadAttention(nn.Module):
     def __init__(
         self,
-        vocab_size: int,
         embed_size: int = 512,
         num_heads: int = 8,
         dropout: float = 0.1,
-        src_mask: torch.tensor = None,
-        device: str = "cpu",
+        src_mask: bool = True,
     ):
-        super(MultiHeadAttention, self).__init__()
-        assert embed_size % num_heads == 0, "embed_size must be divisible by num_heads"
+        super().__init__()
+        assert embed_size % num_heads == 0, "Embedding维度必须可被num_heads整除"
         self.embed_size = embed_size
         self.num_heads = num_heads
         self.src_mask = src_mask
-        self.input_embedding = InputEmbedding(vocab_size, embed_size, device).to(device)
-        self.positional_encoding = PositionalEncoding(embed_size)
-        self.linear_key = nn.Linear(embed_size, embed_size, device=device)
-        self.linear_query = nn.Linear(embed_size, embed_size, device=device)
-        self.linear_value = nn.Linear(embed_size, embed_size, device=device)
+        self.linear_key = nn.Linear(embed_size, embed_size)
+        self.linear_query = nn.Linear(embed_size, embed_size)
+        self.linear_value = nn.Linear(embed_size, embed_size)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, input_x: torch.tensor):
+    def _gen_src_mask(self, src, src_padding_idx: int = 0):
+        # src: [batch_size, seq_len]
+        src_mask = (
+            torch.where((src != src_padding_idx), 0, -1e20).unsqueeze(1).unsqueeze(1)
+        )  # [batch_size, 1, 1, seq_len]如果只保持[batch_size, seq_len]的形状的话，
+        # 无法与[batch_size, num_heads, seq_len, seq_len]形状的attention进行广播
+        return src_mask
+
+    def forward(self, input_x: torch.tensor, x_raw: torch.tensor):
         # input_x: [batch_size, seq_len, embed_size]
         batch_size, seq_len = input_x.shape[0], input_x.shape[1]
         key = self.linear_key(input_x).reshape((batch_size, seq_len, self.num_heads, -1))
@@ -114,8 +88,9 @@ class MultiHeadAttention(nn.Module):
         attention = (
             key.permute(0, 2, 1, 3) @ query.permute(0, 2, 3, 1) / self.embed_size**0.5
         )  # [batch_size, num_heads, seq_len, seq_len]
-        if self.src_mask is not None:
-            attention = attention + self.src_mask
+        if self.src_mask:
+            src_mask = self._gen_src_mask(x_raw)
+            attention = attention + src_mask
         out = (F.softmax(attention, dim=-1) @ value.permute(0, 2, 1, 3)).reshape(
             (batch_size, seq_len, -1)
         )  # out: [batch_size, seq_len, embed_size]
@@ -124,15 +99,11 @@ class MultiHeadAttention(nn.Module):
 
 class FeedForward(nn.Module):
     def __init__(
-        self,
-        embed_size: int = 512,
-        ffn_hidden_size: int = 2048,
-        dropout: float = 0.1,
-        device: torch.device = "cpu",
+        self, embed_size: int = 512, ffn_hidden_size: int = 2048, dropout: float = 0.1
     ):
         super().__init__()
-        self.linear_1 = nn.Linear(embed_size, ffn_hidden_size, device=device)
-        self.linear_2 = nn.Linear(ffn_hidden_size, embed_size, device=device)
+        self.linear_1 = nn.Linear(embed_size, ffn_hidden_size)
+        self.linear_2 = nn.Linear(ffn_hidden_size, embed_size)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, input_x: torch.tensor):
@@ -146,26 +117,24 @@ class FeedForward(nn.Module):
 class EncoderBlock(nn.Module):
     def __init__(
         self,
-        vocab_size: int,
         embed_size: int = 512,
         num_heads: int = 8,
         dropout: float = 0.1,
         ffn_hidden_size: int = 2048,
-        src_mask: torch.tensor = None,
-        device: str = "cpu",
+        src_mask: bool = True,
     ):
         super().__init__()
-        assert embed_size % num_heads == 0, "embed_size must be divisible by num_heads"
+        assert embed_size % num_heads == 0, "Embedding维度必须可被num_heads整除"
         self.multi_head_attention = MultiHeadAttention(
-            vocab_size, embed_size, num_heads, dropout, src_mask, device
+            embed_size, num_heads, dropout, src_mask
         )
-        self.ffn = FeedForward(embed_size, ffn_hidden_size, dropout, device)
-        self.layernorm = nn.LayerNorm(embed_size, device=device)
+        self.ffn = FeedForward(embed_size, ffn_hidden_size, dropout)
+        self.layernorm = nn.LayerNorm(embed_size)
 
     def forward(
-        self, input_x: torch.tensor
+        self, input_x: torch.tensor, x_raw: torch.tensor
     ):  # input_x: [batch_size, seq_len, embed_size]
-        out_1 = self.layernorm(self.multi_head_attention(input_x) + input_x)
+        out_1 = self.layernorm(self.multi_head_attention(input_x, x_raw) + input_x)
         out_2 = self.layernorm(self.ffn(out_1) + out_1)
         return out_2
 
@@ -196,47 +165,30 @@ class Encoder(nn.Module):
         self.ffn_hidden_size = ffn_hidden_size
         self.device = device
 
-        self.input_block = InputBlock(vocab_size, embed_size, device)
-
-    def gen_src_mask(self, src, src_padding_idx: int = 0):
-        # src: [batch_size, seq_len]
-        src_mask = (
-            torch.where((src != src_padding_idx), 0, -1e20).unsqueeze(1).unsqueeze(1)
-        )  # [batch_size, 1, 1, seq_len]如果只保持[batch_size, seq_len]的形状的话，
-        # 无法与[batch_size, num_heads, seq_len, seq_len]形状的attention进行广播
-        return src_mask
-
-    def forward(self, input_x: torch.tensor):
-        # input_x: [batch_size, seq_len]
-        if input_x.shape[1] <= self.max_seq_len:
-            input_x = F.pad(input_x, (0, self.max_seq_len - input_x.shape[1]))
-        else:
-            input_x = input_x[..., : self.max_seq_len]
-        if self.src_mask_flag:
-            src_mask = self.gen_src_mask(input_x)
-        else:
-            src_mask = None
+        self.input_block = InputBlock(vocab_size, embed_size)
 
         self.encoder_block = EncoderBlock(
-            self.vocab_size,
             self.embed_size,
             self.num_heads,
             self.dropout,
             self.ffn_hidden_size,
-            src_mask,
-            device=self.device,
+            src_mask_flag,
         )
         self.encoder = nn.ModuleList([self.encoder_block for _ in range(self.block_num)])
-        x = self.input_block(input_x)
+
+    def forward(self, input_x: torch.tensor):
+        # input_x: [batch_size, seq_len]
+        out = self.input_block(input_x)
         for layer in self.encoder:
-            x = layer(x)
-        return x
+            out = layer(out, input_x)
+        # x = self.encoder(out_1, input_x)
+        return out
 
 
 if __name__ == "__main__":
-    # pass
+
     device = torch.device("mps")
-    input_x = torch.randint(low=0, high=32, size=(32, 32), device=device)
-    encoder = Encoder(vocab_size=32, src_mask_flag=True, device=device)
-    print(encoder(input_x).shape)
-    pass
+    input_x = torch.randint(low=0, high=32, size=(32, 128), device=device)
+    model = Encoder(vocab_size=30000, src_mask_flag=True, device=device)
+    model.to(device)
+    res = model(input_x)
--- dl/transformer/data_tokenize.py
+++ dl/transformer/data_tokenize.py
@@ -1,12 +1,21 @@
-from typing import List
+from typing import List, Literal
 
+import pandas as pd
 import torch
-from datasets import load_dataset
+
+# import torch.utils
+from datasets import (
+    Dataset,
+    DatasetDict,
+    IterableDataset,
+    IterableDatasetDict,
+    load_dataset,
+)
+from sklearn.model_selection import train_test_split
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
-from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.pre_tokenizers import BertPreTokenizer, Whitespace
 from tokenizers.trainers import BpeTrainer
-from torch.utils.data import Dataset
 
 
 def extract_raw_data(data_name: str = "Aye10032/zh-en-translate-20k"):
@@ -31,52 +40,128 @@ def tokenizer_train(corpus: List[str]):
 
 def get_input(tokenizer: Tokenizer, raw_data: List[str]):
     encoding_list = tokenizer.encode_batch(raw_data)
+    tokenizer.encode()
     res = []
     for token in encoding_list:
         res.append(token.ids)
     return res
 
 
-class TranslateDataset(Dataset):
+class TranslateDataset(torch.utils.data.Dataset):
     def __init__(
         self,
-        src: List[int],
-        trg: List[int],
-        src_max_length: int,
-        trg_max_length: int,
-        tokenizer: Tokenizer,
+        data_name: str = "Aye10032/zh-en-translate-20k",
+        src_max_length: int = 128,
+        trg_max_length: int = 128,
+        dateset_type: Literal["train", "eval", "test"] = "train",
+        device: torch.device = "cpu",
     ) -> None:
         super().__init__()
-        self.tokenizer = tokenizer
+        assert dateset_type in [
+            "train",
+            "eval",
+            "test",
+        ], "数据集类型必须为train/eval/test"
+        assert (
+            data_name == "Aye10032/zh-en-translate-20k"
+        ), "暂时只支持Aye10032/zh-en-translate-20k数据集"
+        self.data_name = data_name
         self.src_max_length = src_max_length
         self.trg_max_length = trg_max_length
-        self.src = src
-        self.trg = trg
+        self.dataset_type = dateset_type
+        self.device = device
+
+        self.src, self.trg = self._prepare_dataset()
+
+    def _prepare_dataset(self):
+        # 提取原始翻译语句
+        ds = load_dataset(self.data_name)
+        if len(ds) > 1:
+            res = []
+            for d in ds.values():
+                res.append(d.to_pandas())
+            ds = pd.concat(res)
+        train_eval_ds, test_ds = train_test_split(ds, test_size=0.2)
+        train_ds, eval_ds = train_test_split(train_eval_ds, test_size=0.1)
+        train_src, train_trg, eval_src, eval_trg, test_src, test_trg = (
+            train_ds.iloc[:, 0].to_list(),
+            train_ds.iloc[:, 1].to_list(),
+            eval_ds.iloc[:, 0].to_list(),
+            eval_ds.iloc[:, 1].to_list(),
+            test_ds.iloc[:, 0].to_list(),
+            test_ds.iloc[:, 1].to_list(),
+        )
+        # 构建分词器
+        tokenizer = self._train_tokenizer(ds)
+        # 分词(未填充or截断)
+        if self.dataset_type == "train":
+            src, trg = train_src, train_trg
+        elif self.dataset_type == "eval":
+            src, trg = eval_src, eval_trg
+        else:
+            src, trg = test_src, test_trg
+        src_encoded, trg_encoded = self._encode_corpus(
+            src,
+            trg,
+            tokenizer=tokenizer,
+        )
+
+        return self._add_special_tokens(tokenizer, src_encoded), self._add_special_tokens(
+            tokenizer, trg_encoded
+        )
+
+    def _add_special_tokens(self, tokenizer: Tokenizer, corpus: List[List[int]]):
+        for idx in range(len(corpus)):
+            corpus[idx].insert(0, tokenizer.encode("[BOS]").ids[0])
+            if len(corpus[idx]) >= self.src_max_length:
+                corpus[idx] = corpus[idx][: self.src_max_length]
+            else:
+                while len(corpus[idx]) < self.src_max_length:
+                    corpus[idx].append(tokenizer.encode("[PAD]").ids[0])
+            corpus[idx][-1] = tokenizer.encode("[EOS]").ids[0]
+        return corpus
+
+    def _encode_corpus(
+        self,
+        *src_trg: List[List[str]],
+        tokenizer: Tokenizer = None,
+    ):
+        src_encoded, trg_encoded = [], []
+        src, trg = src_trg
+        for i in range(len(src)):
+            src_encoded.append(tokenizer.encode(src[i]).ids)
+        for i in range(len(trg)):
+            trg_encoded.append(tokenizer.encode(trg[i]).ids)
+        return src_encoded, trg_encoded
+
+    def _train_tokenizer(
+        self, ds: DatasetDict | Dataset | IterableDatasetDict | IterableDataset
+    ) -> Tokenizer:
+        tokenizer = Tokenizer(BPE())
+        tokenizer.pre_tokenizer = BertPreTokenizer()
+        tokenizer_trainer = BpeTrainer(
+            vocab_size=30000,
+            min_frequency=5,
+            show_progress=True,
+            special_tokens=["[PAD]", "[BOS]", "[EOS]", "[MASK]", "[UNK]"],
+        )
+        tokenizer.train_from_iterator(
+            ds.iloc[:, 0].to_list() + ds.iloc[:, 1].to_list(), tokenizer_trainer
+        )
+
+        return tokenizer
 
     def __getitem__(self, index: int):
-        src = self.src[index]
-        trg = self.trg[index]
-        # 向目标序列的句首添加[BOS]，句尾添加[EOS]
-        trg.insert(0, self.tokenizer.encode("[BOS]").ids[0])
-        trg.append(self.tokenizer.encode("[EOS]").ids[0])
-        if len(src) > self.src_max_length:
-            src = src[: self.src_max_length]
-        if len(trg) > self.trg_max_length:
-            trg = trg[: self.trg_max_length]
 
-        while len(src) < self.src_max_length:
-            src.append(self.tokenizer.encode("[PAD]").ids[0])
-        while len(trg) < self.trg_max_length:
-            trg.append(self.tokenizer.encode("[PAD]").ids[0])
-
-        return torch.tensor(src), torch.tensor(trg)
+        return torch.tensor(self.src[index], device=self.device), torch.tensor(
+            self.trg[index], device=self.device
+        )
 
     def __len__(self):
         return len(self.src)
 
 
 if __name__ == "__main__":
-    corpus = extract_raw_data()
-    tokenizer = tokenizer_train(corpus)
-    res = get_input(tokenizer, corpus)
+    device = torch.device("mps")
+    trans_dataset = TranslateDataset(device=device)
     pass
